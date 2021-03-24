2d74e9b24cfa6ce8828481779da1a98c0430c440
winson65 <winson.yu22@gmail.com>
2021-03-24 19:23:05 -0400

    implemented fill

--- src/components/TableCell.js
+++ src/components/TableCell.js
@@ -1,5 +1,11 @@
+import { BackgroundColor } from "chalk";
+
 function TableCell(props) {
-  return <td onClick={props.handleApplyColor}></td>
+  return <td onClick = {props.handleApplyColor}
+    style = {{
+        backgroundColor : 'transparent'
+    }}> 
+    </td>
 }
 
 export default TableCell;
